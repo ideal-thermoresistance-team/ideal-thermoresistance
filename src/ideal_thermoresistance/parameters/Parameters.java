a27957e3f329aafd6f5591bd6a3b83f6cff8bb77
Pavel Cherenkov <cherenkov.p.a@yandex.ru>
2014-12-04 18:43:59 +0600

    Implemented Parameters.

--- src/ideal_thermoresistance/parameters/Parameters.java
+++ src/ideal_thermoresistance/parameters/Parameters.java
@@ -1,15 +1,43 @@
 package ideal_thermoresistance.parameters;
 
+import java.util.HashMap;
+import java.util.Observable;
+
 import ideal_thermoresistance.functions.Function;
 
-public class Parameters {
+public class Parameters extends Observable {
+	private HashMap<DoubleParameterName, Double> doubleVals;
+	private HashMap<BooleanParameterName, Boolean> boolVals;
+	private Function func;
+	
 	public Function getFunction() {
-		return null;
+		return func;
 	}
 	public double getDouble(DoubleParameterName name) {
-		return 0;
+		return doubleVals.get(name);
 	}
 	public boolean getBoolean(BooleanParameterName name) {
-		return false;
+		return boolVals.get(name);
+	}
+	
+	public void setFunction(Function function)
+	{
+		func = function;
+	}
+	
+	public void setDouble(DoubleParameterName name, double value)
+	{
+		doubleVals.put(name, value);
+	}
+	
+	public void setBoolean(BooleanParameterName name, boolean value)
+	{
+		boolVals.put(name, value);
+	}
+	
+	public void update()
+	{
+		setChanged();
+		notifyObservers();
 	}
 }
