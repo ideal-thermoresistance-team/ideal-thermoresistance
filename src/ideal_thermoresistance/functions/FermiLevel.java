53f1c0608db670baf56215907f684217f7e6326a
frostuser <redthatmadcat@gmail.com>
2014-12-12 02:37:50 +0600

    1. Moved necessary steps for Fermi level computation into the separate class. 2. Slightly refactored previously mentioned steps. 3. C_n, C_p, T_n0, T_p0 are now read from the Parameters class.

---
+++ src/ideal_thermoresistance/functions/FermiLevel.java
@@ -0,0 +1,56 @@
+package ideal_thermoresistance.functions;
+
+import ideal_thermoresistance.parameters.DoubleParameterName;
+import ideal_thermoresistance.parameters.Parameters;
+
+/**
+ * Attention! The function returns not the actual fermi level, but exp(mu / (k*T)).
+ * This is done to avoid unnecessary  
+ */
+public class FermiLevel implements Function {
+	
+	public double compute(Parameters params, double T) {
+		double Eg = params.getDouble(DoubleParameterName.Eg);
+		double Ed1 = params.getDouble(DoubleParameterName.Ed1);
+		double Ed2 = params.getDouble(DoubleParameterName.Ed2);
+		double Nd1 = params.getDouble(DoubleParameterName.Nd1);
+		double Nd2 = params.getDouble(DoubleParameterName.Nd2);
+		
+		double me = params.getDouble(DoubleParameterName.me);
+		double mh = params.getDouble(DoubleParameterName.mh);
+		
+		// TODO: Move constants to the happier land.
+		double k  = 1.38e-16;
+		double m0 = 9.1e-28;
+		
+		double NC = 2.51e19 * Math.pow(me/m0 * T/300, 1.5);
+		double NV = 2.51e19 * Math.pow(mh/m0 * T/300, 1.5);
+		
+		double q0 = Math.exp(Eg / (k*T));
+		double q1 = Math.exp((-Ed1) / (k*T));
+		double q2 = Math.exp((-Ed2) / (k*T));
+		double a = (Nd1*q2 + Nd2*q1) - (NC*(q1 + q2) - NV);
+		double b = NC*q1*q2 - NV*(q1 + q2);
+		double c = NV*q1*q2;
+		double denom = (Nd1 + Nd2)*q0 - NC;
+		
+		a /= denom;
+		b /= denom;
+		c /= denom;
+		
+		CubicPolynomial poly = new CubicPolynomial(a, b, c);
+		
+		double[] roots = poly.getRoots();
+		double root = 0;
+		
+		for (int i = 0; i < roots.length; ++i) {
+			if (roots[i] > 0 && !Double.isNaN(roots[i])) {
+				root = roots[i];
+				break;
+			}
+		}
+		
+		return root / q0;
+	}
+
+}
--- src/ideal_thermoresistance/functions/Resistance.java
+++ src/ideal_thermoresistance/functions/Resistance.java
@@ -5,6 +5,8 @@ import ideal_thermoresistance.parameters.Parameters;
 
 public class Resistance implements Function {
 
+	private FermiLevel fermi_level = new FermiLevel();
+
 	public double compute(Parameters params, double T) {
 		double Eg = params.getDouble(DoubleParameterName.Eg);
 		double Ed1 = params.getDouble(DoubleParameterName.Ed1);
@@ -24,30 +26,10 @@ public class Resistance implements Function {
 		double NV = 2.51e19 * Math.pow(mh/m0 * T/300, 1.5);
 		
 		double q0 = Math.exp(Eg / (k*T));
-		double q1 = q0 * Math.exp(-Ed1 / (k*T));
-		double q2 = q0 * Math.exp(-Ed2 / (k*T));
+		double q1 = Math.exp((Eg-Ed1) / (k*T));
+		double q2 = Math.exp((Eg-Ed2) / (k*T));
 		
-		double a = (Nd1*q2 + Nd2*q1) - (NC*(q1 + q2) - NV*q0);
-		double b = NC*q1*q2 - NV*q0*(q1 + q2);
-		double c = NV*q0*q1*q2;
-		
-		double denom = (Nd1 + Nd2)*q0 - NC;
-		
-		a /= denom;
-		b /= denom;
-		c /= denom;
-		
-		CubicPolynomial poly = new CubicPolynomial(a, b, c);
-		
-		double[] roots = poly.getRoots();
-		double root = 0;
-		
-		for (int i = 0; i < roots.length; ++i) {
-			if (roots[i] > 0 && !Double.isNaN(roots[i])) {
-				root = roots[i];
-				break;
-			}
-		}
+		double root = fermi_level.compute(params, T);
 		
 		double n = NC * root / q0;
 		double p = NV / root;
@@ -55,13 +37,12 @@ public class Resistance implements Function {
 		double Nd1_c = Nd1 / (1 + q1 / root);
 		double Nd2_c = Nd2 / (1 + q2 / root);
 		
-		// TODO: These parameters must be set by user.
-		double Cn = 1.0;
-		double Cp = 1.0;
-		double T0n = 1.0;
-		double T0p = 1.0;
+		double Cn = params.getDouble(DoubleParameterName.Cn);
+		double Cp = params.getDouble(DoubleParameterName.Cn);
+		double T0n = params.getDouble(DoubleParameterName.T0n);
+		double T0p = params.getDouble(DoubleParameterName.T0p);
 		
-		
+		// TODO: These formulae are operated in International System of Units
 		double mu_n = Cn / (Math.pow(T/T0n, 1.5) + 
 				(Nd1_c + Nd2_c + p)*Math.pow(T0n/T, 1.5));
 		double mu_p = Cp / (Math.pow(T/T0p, 1.5) + 
